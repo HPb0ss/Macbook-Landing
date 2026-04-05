fe287a8b0f04e6564d76bbb400cb6297905a7c15
HPb0ss <hetampattel1126@gmail.com>
2026-04-05 20:57:01 +0530

    Implemented Features section

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import { texture } from "three/tsl";
 import { create } from "zustand";
 
 const useMacbookStore = create((set) => ({
@@ -13,12 +14,17 @@ const useMacbookStore = create((set) => ({
     sizeInInches: '16"',
     setSizeInInches: (sizeInInches) => set({ sizeInInches }),
 
+    texture: "/videos/feature-1.mp4",
+    setTexture: (texture) => set({ texture }),
+
     reset: () =>
         set({
             color: "#2e2c2e",
             colorName: "Dark",
             scale: 0.08,
             sizeInInches: '16"',
-        }),}));
+            texture: "/videos/feature-1.mp4",
+        }),
+}));
 
 export default useMacbookStore;
